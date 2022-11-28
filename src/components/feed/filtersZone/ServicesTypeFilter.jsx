8c7de7bf8d96a3519e4e3e5cb9aa6bf0e73b4239
Alejandro <alejandrom.am33@gmail.com>
2022-11-27 23:46:36 -0600

    add partial functionality in the filter of service

--- src/components/feed/filtersZone/ServicesTypeFilter.jsx
+++ src/components/feed/filtersZone/ServicesTypeFilter.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {IoIosArrowDown} from 'react-icons/io'
 
 export default function ServicesTypeFilter() {
+  const [menuOpen, setMenuOpen] = useState(false)
+  const [indexOptionActive, setIndexOptionActive] = useState(0)
+
+  const options = ['Todos los servicios', 'Pagados', 'Por pagar']
+  
+  const handleMenu = () => setMenuOpen(!menuOpen)
+  const handleMenuOption = (e) => 
+    setIndexOptionActive(options.indexOf(e.target.innerHTML))
+  
   return (
-    <div className='services-type-filter-container'>
-      <h3>Todos los servicios</h3>
+    <div 
+      onClick={handleMenu} 
+      className={`services-type-filter-container ${menuOpen ? 'menu-filter-type-open' : ''}`}
+    >
+      <h3>{options[indexOptionActive]}</h3>
       <IoIosArrowDown className='services-type-filter-icon'/>
+      <nav className='services-type-menu'>
+        <ul>
+          {options.map((option, index) => {
+            return (
+              <li 
+                className={`${indexOptionActive === index ? 'option-selected' : ''}`} 
+                onClick={handleMenuOption} 
+                key={index}
+              >
+                {option}
+              </li>
+            )
+          })}
+        </ul>
+      </nav>
     </div>
   )
 }
--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -6,7 +6,7 @@ import Header from '../components/feed/header/Header'
 export default function Feed() {
 
   return (
-    <div>
+    <div style={{paddingBottom: 3000,}}>
       <Header/>
       <Filters/>
       <AddServiceButton/>
