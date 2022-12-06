19b7f6620375f5b61f4caf40467a07306d23c585
Alejandro <alejandrom.am33@gmail.com>
2022-12-06 11:18:24 -0600

    add subscription cards in feed

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -1,14 +1,37 @@
 import React from 'react'
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import AddServiceButton from '../components/feed/AddServiceButton'
 import Filters from '../components/feed/filtersZone/Filters'
 import Header from '../components/feed/header/Header'
+import SubscriptionCard from '../components/feed/subscriptionCard/SubscriptionCard'
+import SubscriptionCardLoading from '../components/feed/subscriptionCard/SubscriptionCardLoading'
+import { readUserSubscriptions, selectUserSubscriptions, selectUserSubscriptionsLoading } from '../features/subscriptions/subscriptionsSlice'
+import { selectUserData } from '../features/user/userSlice'
 
 export default function Feed() {
+  const dispatch = useDispatch()
+  const uid = useSelector(selectUserData)
+  const subs = useSelector(selectUserSubscriptions)
+  const subsLoading = useSelector(selectUserSubscriptionsLoading)
+
+  useEffect(() => {
+    dispatch(readUserSubscriptions(uid.uid))
+  }, [uid])
+  
 
   return (
-    <div style={{paddingBottom: 3000,}}>
+    <div style={{minHeight: '100vh',}}>
       <Header/>
       <Filters/>
+      <div className='subscriptions-container'>
+        {subsLoading && <SubscriptionCardLoading/>}
+        {subs.map(sub => {
+          return (
+            <SubscriptionCard key={sub.id} subscription={sub}/>
+          )
+        })}
+      </div>
       <AddServiceButton/>
     </div>
   )
