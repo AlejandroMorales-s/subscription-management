50b10b4efdee53ceb3e77b4e19d1fa6d769033c0
Alejandro <alejandrom.am33@gmail.com>
2022-12-06 12:58:35 -0600

    add reducer to set total amount in price filter

--- src/features/filters/filtersSlice.js
+++ src/features/filters/filtersSlice.js
@@ -1,16 +1,50 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  priceFilter: {},
+  priceFilter: {
+    filterType: "",
+    newTotalAmountToPayFiltered: 0,
+    totalAmount: 0,
+  },
   subscriptionsFilter: {},
 };
 
 const options = {
   name: "filters",
   initialState,
-  reducers: {},
+  reducers: {
+    setTotalAmountToPay: (state, action) => {
+      const subscriptions = action.payload;
+
+      const pricesArray = [];
+
+      subscriptions.forEach((sub) => pricesArray.push(Number(sub.data.price)));
+
+      state.priceFilter.totalAmount = pricesArray.reduce(
+        (accumulator, currentValue) => accumulator + currentValue
+      );
+    },
+    /*
+    modifyPriceFilter: (state, action) => {
+      const { filterTypeName, filterTypeIndex, subscriptions } = action.payload;
+      
+      state.priceFilter.filterType = filterTypeName;
+      
+      if (filterTypeIndex === 0) {
+      }
+    },
+    */
+  },
 };
 
 const filtersSlice = createSlice(options);
+
+//* Exporting reducers
+export const { setTotalAmountToPay } = filtersSlice.actions;
+
+//* Selectors
+export const selectPriceFilterInfo = (state) => state.filters.priceFilter;
+export const selectSubscriptionsFilterInfo = (state) =>
+  state.filters.subscriptionsFilter;
 
 export default filtersSlice.reducer;
