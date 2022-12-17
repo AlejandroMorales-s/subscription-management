2feee12954a60d9e404a5480d0697c2d82aaf17d
Alejandro <alejandrom.am33@gmail.com>
2022-12-17 14:45:47 -0600

    functionality to check if user is logged

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,41 @@
 import './App.scss';
-import {Routes, Route} from 'react-router-dom';
+import {Routes, Route, useNavigate, useLocation} from 'react-router-dom';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
 import Feed from './pages/Feed';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { authChangeHandler } from './features/user/userSlice';
+import { auth } from './libs/firebase';
 
 function App() {
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const userLocation = useLocation()
+
+  useEffect(() => {
+    const availablePagesIfUserIsNotLogged = [
+      '/', 
+      '/signup'
+    ]
+    
+    dispatch(authChangeHandler(auth))
+    .then(res => {
+      const currentLocation = userLocation.pathname 
+      if (
+        !res.error
+        && availablePagesIfUserIsNotLogged.includes(currentLocation)
+        ) {
+          navigate('/feed')
+      } else if (
+          res.error.message === 'Session closed' 
+          && !availablePagesIfUserIsNotLogged.includes(currentLocation)
+        ){  
+          navigate('/')
+      } 
+    })
+  }, [])
+  
   return (
     <Routes>
       <Route path='/' element={<Login/>}/>
