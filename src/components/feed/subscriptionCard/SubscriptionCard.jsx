1fe762111ddad4a0f355a084e2655a099f5a2d87
Alejandro <alejandrom.am33@gmail.com>
2022-12-31 17:45:56 -0600

    add change of the text color

--- src/components/feed/subscriptionCard/SubscriptionCard.jsx
+++ src/components/feed/subscriptionCard/SubscriptionCard.jsx
@@ -1,34 +1,45 @@
 import React from 'react'
-import { useEffect } from 'react'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import {BsThreeDotsVertical} from 'react-icons/bs'
 import { useSelector } from 'react-redux'
 import { selectPriceFilterInfo } from '../../../features/filters/filtersSlice'
+import getContrast from '../../../utils/getContrastBetweenTwoColors'
 import DeleteSubscriptionModal from './DeleteSubscriptionModal'
 
 export default function SubscriptionCard({subscription}) {
   const { name, price, color } = subscription.data
 
+  //* States
   const [menuOpen, setMenuOpen] = useState(false)
   const [deleteSubModal, setDeleteSubModal] = useState(false)
   const [priceController, setPriceController] = useState(price)
-
+  const [textColor, setTextColor] = useState('#000000')
+  
+  //* Selectors
   const filterInfo = useSelector(selectPriceFilterInfo) 
-
+  
+  //* Use effect
   useEffect(() => {
     if (filterInfo.filterType ==='Semanal') setPriceController(price / 4)
     else if (filterInfo.filterType ==='Mensual') setPriceController(price)
     else if (filterInfo.filterType ==='Anual') setPriceController(price * 12)
+  
+    if (getContrast(color) > 10) setTextColor('#000000')
+    else setTextColor('#ffffff')
   }, [filterInfo.filterType])
 
 
   return (
     <>
-      <div className={`subscription-card ${color}`}>
-        <h3>{name}</h3>
+      <div 
+        style={{backgroundColor: color}}
+        className='subscription-card'
+      >
+        <h3 style={{color: textColor}}>{name}</h3>
         <div className='subscription-card-price-container'>
-          <h2>${priceController}</h2> 
+          <h2 style={{color: textColor}}>${priceController}</h2> 
           <BsThreeDotsVertical
+            style={{fill: textColor}}
             className='subscription-card-icon'
             onClick={() => setMenuOpen(!menuOpen)}
           />
